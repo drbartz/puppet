384dea27bc4d0ffd1d5962e5ae84b4e06a8beeb2
Mateus Bartz <mateus.bartz@gmail.com>
2014-12-18 18:22:44 +0100

    graphite by git

--- manifests/nodes/graphite.pp
+++ manifests/nodes/graphite.pp
@@ -1,8 +1,68 @@
-node /graphite.*[0-9][0-9]$/ {
-	#package { [ 'epel-release', 'graphite-web', 'python-carbon']:
-	#	ensure => installed,
-	#}
+node /graphite01.*$/ {
+	package { [ 'epel-release', 'graphite-web', 'python-carbon',  'httpd', 'mod_wsgi']:
+		ensure => installed,
+	}
 	include basic
 	include puppet::client
-	include graphite
+	#include graphite
+}
+
+node /graphite02.*/ {
+	package { [ 'epel-release', 'git' , 'libffi-devel', 'httpd', 'mod_wsgi' ]:
+		ensure => installed,
+	}
+	include basic
+	include puppet::client
+	#include graphite
+
+   file { '/opt/graphite':
+   	ensure    => directory,
+		mode      => '0644',
+		owner     => 'root',
+		group     => 'root',
+	}
+
+   file { '/opt/graphite/requirements.txt':
+   	ensure    => present,
+      content   => file('graphite/requirements.txt'),
+		mode      => '0644',
+		owner     => 'root',
+		group     => 'root',
+	}
+
+   file { '/etc/httpd/conf.d/graphite.conf':
+   	ensure    => present,
+      content   => file('graphite/http_graphite.conf'),
+		mode      => '0644',
+		owner     => 'root',
+		group     => 'root',
+		require => Package['httpd'],
+	}
+
+	class { 'python' :
+		version    => 'system',
+		pip        => true,
+		dev        => true,
+		virtualenv => false,
+		gunicorn   => false,
+	}
+
+	python::requirements { '/opt/graphite/requirements.txt' :
+		owner      => 'root',
+	}
+
+   python::pip { ['whisper', 'carbon', 'graphite-web']:
+	  ensure		=> present,
+   }
+
+	exec { "/usr/bin/python /opt/graphite/webapp/graphite/manage.py syncdb":
+		cwd     => "/opt/graphite",
+		path    => ["/usr/bin", "/usr/sbin"]
+	}
+
+	service { 'httpd':
+		ensure => running,
+		enable => true,
+	}
+
 }
