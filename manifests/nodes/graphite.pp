32b272f9e4ed7dcb8efceb8212f1940011eaf999
Mateus Bartz <mateus.bartz@gmail.com>
2015-01-08 00:46:42 +0100

    add zabbix::agent

--- manifests/nodes/graphite.pp
+++ manifests/nodes/graphite.pp
@@ -3,21 +3,5 @@ node /graphite01.*/ {
 	include basic
 	include puppet::client
 	include graphite::server_git
-	#include graphite::install_from_git
-	#include graphite::config
-	#Class['graphite::install_from_git'] -> Class['graphite::config']
-}
-
-# install graphite from epel rpm packages
-node /graphite02.*$/ {
-	package { [ 'epel-release', 'httpd', 'mod_wsgi']:
-		ensure => installed,
-	}
-	package { [ 'graphite-web', 'python-carbon' ]:
-		ensure => installed,
-		require => Package['epel-release'],
-	}
-	include basic
-	include puppet::client
-	#include graphite
+	#include zabbix::agent
 }
