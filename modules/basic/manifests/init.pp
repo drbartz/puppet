7a6e590dc608c821ecb0981d4b9e1700de2526d8
Mateus Bartz <mateus.bartz@gmail.com>
2015-01-07 20:05:21 +0100

    add nc, telnet and rsync packages

--- modules/basic/manifests/init.pp
+++ modules/basic/manifests/init.pp
@@ -2,7 +2,7 @@ class basic {
 	include basic::localtime
 	include basic::hosts
 	include basic::vim
-	package { ['telnet', 'bind-utils']:
+	package { ['telnet', 'bind-utils', 'nc', 'telnet', 'rsync']:
 		ensure	=> present,
 	}
 }
