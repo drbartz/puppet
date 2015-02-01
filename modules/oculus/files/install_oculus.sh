71d4ff35e23aded6ced6010e293ab2057c7d0cdd
Mateus Bartz <mateus.bartz@gmail.com>
2015-02-01 08:15:05 +0100

    Oculus fix

--- modules/oculus/files/install_oculus.sh
+++ modules/oculus/files/install_oculus.sh
@@ -4,7 +4,12 @@ TMP_DIR=/tmp/oculus
 OUTPUT_FILE=${TMP_DIR}/.install.done
 PACK_FILE="/vagrant/skyline_python_pack.1.0.1.tgz"
 SKYLINE_GIT_SOURCE="https://github.com/drbartz/skyline -b devel"
-SET HTTP_PROXY=http://10.10.10.2:3128
+set HTTP_PROXY=http://10.10.10.2:3128
+export http_proxy=http://10.10.10.2:3128
+
+RUBY_PUPPET_VER=1.8.7
+RUBY_OCULUS_VER=2.1.2
+ELASTICSEARCH_VER=0.90.3
 
 # clean and prepare temp dir
 [ -d ${TMP_DIR} ] && rm -rf ${TMP_DIR}
@@ -19,43 +24,42 @@ then
 	tar -zxf ${PACK_FILE}
 fi
 
-# install ruby version 2.1.2 (oculus) and 1.8.7 (puppet)
+# install ruby version ${RUBY_OCULUS_VER} (oculus) and 1.8.7 (puppet)
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
-rvm install 2.1.2
-rvm install 1.8.7
+rvm install ${RUBY_OCULUS_VER}
+rvm install ${RUBY_PUPPET_VER}
 
 # prepare puppet env
-rvm use 1.8.7
-gem isntall json
+rvm use ${RUBY_PUPPET_VER}
+gem install json
 
 #download oculus gig
 git clone https://github.com/etsy/oculus.git
 
 #elasticsearch install
-rmv use 2.1.2
+rmv use ${RUBY_OCULUS_VER}
 
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.0.tar.gz
-tar -zxvf elasticsearch-0.90.0.tar.gz
-cd elasticsearch-0.90.0
+ELASTICSEARCH_VER="0.90.3"
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${ELASTICSEARCH_VER}.tar.gz
+tar -zxvf elasticsearch-${ELASTICSEARCH_VER}.tar.gz
+cd elasticsearch-${ELASTICSEARCH_VER}
 cp -a ../oculus/resources/elasticsearch-oculus-plugin .
 cd elasticsearch-oculus-plugin/
 rake build
-cp -a OculusPlugins.jar /opt/elasticsearch-0.90.0/lib/.
+cp -a OculusPlugins.jar /opt/elasticsearch-${ELASTICSEARCH_VER}/lib/.
 
 # oculus install
 cd ${TMP_DIR}/oculus
 gem install bundler
 bundle install
 
-
 # move to the final destination (/opt)
 cd ${TMP_DIR}
 [ -d '/opt' ] || mkdir /opt
-[ -d '/opt/elasticsearch-0.90.0' ] && rm -rf /opt/elasticsearch-0.90.0
-mv elasticsearch-0.90.0 /opt/.
+[ -d "/opt/elasticsearch-${ELASTICSEARCH_VER}" ] && rm -rf /opt/elasticsearch-${ELASTICSEARCH_VER}
+mv elasticsearch-${ELASTICSEARCH_VER} /opt/.
 [ -d '/opt/oculus' ] && rm -rf /opt/oculus
 mv oculus /opt/.
 mv ${OUTPUT_FILE} /opt/oculus/.
-
--- modules/oculus/files/oculus_import.sh
+++ modules/oculus/files/oculus_import.sh
@@ -2,14 +2,15 @@
 
 LOG=/var/log/oculus/import.log
 
-PATH=/usr/local/rvm/gems/ruby-2.1.2/bin:/usr/local/rvm/gems/ruby-2.1.2@global/bin:/usr/local/rvm/rubies/ruby-2.1.2/bin:/usr/local/rvm/bin:/usr/bin:/sbin:/bin:/usr/sbin
-GEM_HOME=/usr/local/rvm/gems/ruby-2.1.2
-IRBRC=/usr/local/rvm/rubies/ruby-2.1.2/.irbrc
-MY_RUBY_HOME=/usr/local/rvm/rubies/ruby-2.1.2
-GEM_PATH=/usr/local/rvm/gems/ruby-2.1.2:/usr/local/rvm/gems/ruby-2.1.2@global
-RUBY_VERSION=ruby-2.1.2
+PATH=/usr/local/rvm/gems/ruby-1.9.2-p330/bin:/usr/local/rvm/gems/ruby-1.9.2-p330@global/bin:/usr/local/rvm/rubies/ruby-1.9.2-p330/bin:/usr/local/rvm/bin:/usr/bin:/sbin:/bin:/usr/sbin
+GEM_HOME=/usr/local/rvm/gems/ruby-1.9.2-p330
+IRBRC=/usr/local/rvm/rubies/ruby-1.9.2-p330/.irbrc
+MY_RUBY_HOME=/usr/local/rvm/rubies/ruby-1.9.2-p330
+GEM_PATH=/usr/local/rvm/gems/ruby-1.9.2-p330:/usr/local/rvm/gems/ruby-1.9.2-p330@global
+RUBY_VERSION=ruby-1.9.2-p330
 
 export PATH GEM_HOME IRBRC MY_RUBY_HOME GEM_PATH RUBY_VERSION
 
 date >> ${LOG}
-/opt/oculus/scripts/import.rb >> ${LOG} 2>> ${LOG}
+cd /opt/oculus/scripts/
+./import.rb >> ${LOG} 2>> ${LOG}
