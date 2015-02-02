971207bcddddf32980cf8a48c2db3407405081fb
Mateus Bartz <mateus.bartz@gmail.com>
2015-02-02 15:48:48 +0100

    fix

--- modules/puppet/manifests/client.pp
+++ modules/puppet/manifests/client.pp
@@ -1,22 +1,10 @@
 class puppet::client {
+    include puppet::client_fast
 	file {'/etc/puppet/puppet.conf':
 		ensure => present,
 		content   => file('puppet/puppet.conf'),
 		notify => Service['puppet'],
 	}
-
-#    file {'/etc/yum.conf':
-#        ensure  => present,
-#        content => file('puppet/client_yum.conf'),
-#    }
-
-#    file {'/etc/yum.repos.d/autocache.repo':
-#        ensure  => present,
-#        owner   => 'root',
-#        group   => 'root',
-#        mode    => '0644',
-#        content => file('puppet/autocache.repo'),
-#    }
 
 	package {'puppet':
 		ensure => installed, 
