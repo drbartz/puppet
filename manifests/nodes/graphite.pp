5a68601ffa9fba57fea8487b34c3367d099e5ec4
Mateus Bartz <mateus.bartz@gmail.com>
2015-02-16 17:54:42 +0100

    merge file

--- manifests/nodes/graphite.pp
+++ manifests/nodes/graphite.pp
@@ -1,17 +1,9 @@
 # install graphite from git 
 node /graphite01.*/ {
-<<<<<<< HEAD
-	include basic
-	include puppet::client
-	include graphite::server_git
-	include graphite::collect
-	#include zabbix::agent 			#discoment if you have the Zabbix server installed
-=======
     include basic
     include puppet::client
     include graphite::server_git
     include graphite::diamond
     #include zabbix::agent
     Class['puppet::client'] -> Class['basic'] -> Class['graphite::server_git'] 
->>>>>>> graphite_skyline
 }
