4199f804b116d34baef8e529b2c4de0268909cb1
Mateus Bartz <mateus.bartz@gmail.com>
2015-03-06 11:57:26 +0100

    change graphite to support puppet/hiera config

--- manifests/nodes/graphite.pp
+++ manifests/nodes/graphite.pp
@@ -2,12 +2,12 @@
 node /graphite0.*/ {
     include basic
     include puppet::client
-    include graphite::server_git
+    include graphite::server
     include graphite::diamond
     include elasticsearch
     include grafana
     #include rsyslog
     #include logstash::forwarder
     #include zabbix::agent
-    Class['puppet::client'] -> Class['basic'] -> Class['graphite::server_git'] 
+    Class['puppet::client'] -> Class['basic'] -> Class['graphite::server'] 
 }
