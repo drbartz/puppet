7aaacfdede54ac4a27f9e17d806e94a229a1f09a
Mateus Bartz <mateus.bartz@gmail.com>
2015-12-16 07:49:06 +0100

    fix zabbix install

--- modules/zabbix/files/install_zabbix_server.sh
+++ modules/zabbix/files/install_zabbix_server.sh
@@ -8,11 +8,11 @@ grant all privileges on zabbix.* to 'zabbix'@'localhost' identified by '${Zabbix
 flush privileges;
 __END__
 
-mysql -u zabbix -p${Zabbix_Pass} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.3/create/schema.sql
+mysql -u zabbix -p${Zabbix_Pass} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.7/create/schema.sql
 
-mysql -u zabbix -p${Zabbix_Pass} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.3/create/images.sql
+mysql -u zabbix -p${Zabbix_Pass} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.7/create/images.sql
 
-mysql -u zabbix -p${Zabbix_Pass} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.3/create/data.sql
+mysql -u zabbix -p${Zabbix_Pass} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.7/create/data.sql
 
 cd /etc/zabbix
 git clone https://github.com/drbartz/zabbix_api.git
