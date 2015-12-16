3c1f8c9011d0d301e9041e54c27f0dc5fa05b016
root <root@puppet.(none)>
2015-12-16 07:27:02 +0100

    fix diamond pre-req install

--- modules/graphite/files/install_diamond.sh
+++ modules/graphite/files/install_diamond.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#pre-req
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python
+
 cd /tmp
 git clone https://github.com/python-diamond/Diamond
 cd Diamond/
