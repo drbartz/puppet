c7a3b1fba920a2525ff9d806a353dde1e7fc5f5c
Mateus Bartz <mateus.bartz@gmail.com>
2016-12-27 18:14:17 +0100

    errbot first ok

--- modules/errbot/files/install_errbot.sh
+++ modules/errbot/files/install_errbot.sh
@@ -3,6 +3,6 @@
 yum update -y
 yum install -y python34 python34-devel  python34-setuptools python-pip
 yum install -y gcc libffi-devel python-devel openssl-devel
-easy_install-3.4 errbot errbot[telegram]
+easy_install-3.4 errbot errbot[telegram]  errbot[slack]
 
 date > /etc/errbot/.install_errbot.done
